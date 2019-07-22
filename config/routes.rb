8b12bcde483ce1da2dc74371a577009a72a08654
Lisa Antwi Adjei <lisaantwi@gmail.com>
2019-07-22 19:15:15 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  get 'school_classes/index'
+  # get 'school_classes/index'
+  #
+  # get 'school_classes/new'
+  #
+  # get 'school_classes/create'
+  #
+  # get 'school_classes/update'
+  #
+  # get 'school_classes/show'
+  #
+  # get 'students/index'
 
-  get 'school_classes/new'
-
-  get 'school_classes/create'
-
-  get 'school_classes/update'
-
-  get 'school_classes/show'
-
-  get 'students/index'
+  resources :students, except: [:destroy, :index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
