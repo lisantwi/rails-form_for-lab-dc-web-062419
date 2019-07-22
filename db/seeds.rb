4443dd73bea138062de1388dd7a3cf218dceea40
Lisa Antwi Adjei <lisaantwi@gmail.com>
2019-07-22 19:31:55 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Student.destroy_all
-lisa = Student.find_or_create(first_name: "Lisa", last_name:"Antwi Adjei")
-beyonce = Student.find_or_create(first_name: "Beyonce", last_name:"Knowles")
-mariah = Student.find_or_create(first_name: "Mariah", last_name:"Carey")
-nicki = Student.find_or_create(first_name:"Nicki", last_name: "Minaj")
+lisa = Student.find_or_create_by(first_name: "Lisa", last_name:"Antwi Adjei")
+beyonce = Student.find_or_create_by(first_name: "Beyonce", last_name:"Knowles")
+mariah = Student.find_or_create_by(first_name: "Mariah", last_name:"Carey")
+nicki = Student.find_or_create_by(first_name:"Nicki", last_name: "Minaj")
