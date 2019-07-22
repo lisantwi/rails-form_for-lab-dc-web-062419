5c65e5e03cf7fcf063bf784f6dcc7a53e9f69365
Lisa Antwi Adjei <lisaantwi@gmail.com>
2019-07-22 19:30:35 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,14 @@
 class StudentsController < ApplicationController
   def new
+    
   end
 
   def create
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_names]
+    @student.save
+    redirect_to student_path(@student)
   end
 
   def show
