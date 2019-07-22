2eb71ba68ce0ae6910abf050a672781ad426700f
Lisa Antwi Adjei <lisaantwi@gmail.com>
2019-07-22 19:12:14 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'school_classes/index'
+
+  get 'school_classes/new'
+
+  get 'school_classes/create'
+
+  get 'school_classes/update'
+
+  get 'school_classes/show'
+
   get 'students/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
