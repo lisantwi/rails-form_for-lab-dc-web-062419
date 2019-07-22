5214bd5ccbe66f523ff529b85ed67aed8944d33a
Lisa Antwi Adjei <lisaantwi@gmail.com>
2019-07-22 21:13:55 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -23,5 +23,6 @@ class StudentsController < ApplicationController
   def update
       @student = Student.find(params[:id])
       @student.update(first_name: params[:first_name], last_name: params[:last_name])
+      redirect_to student_path(@student)
   end
 end
