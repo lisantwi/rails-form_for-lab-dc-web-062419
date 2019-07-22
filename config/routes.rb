b35905ce10f4c2d31f6dd56ff8c86e892814f80a
Lisa Antwi Adjei <lisaantwi@gmail.com>
2019-07-22 21:22:27 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
+  get 'students/new'
+
+  get 'students/create'
+
+  get 'students/show'
+
+  get 'students/edit'
+
+  get 'students/update'
+
+  # get 'school_classes/index'
+  #
+  # get 'school_classes/new'
+  #
+  # get 'school_classes/create'
+  #
+  # get 'school_classes/update'
+  #
+  # get 'school_classes/show'
+  #
+  # get 'students/index'
+
+  resources :students, except: [:destroy, :index]
+  resources :school_classes, except: [:destroy, :index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.destroy_all
+lisa = Student.find_or_create_by(first_name: "Lisa", last_name:"Antwi Adjei")
+beyonce = Student.find_or_create_by(first_name: "Beyonce", last_name:"Knowles")
+mariah = Student.find_or_create_by(first_name: "Mariah", last_name:"Carey")
+nicki = Student.find_or_create_by(first_name:"Nicki", last_name: "Minaj")
