4f8808f617195043cc6ca69cfbaae6adc60c5157
Lisa Antwi Adjei <lisaantwi@gmail.com>
2019-07-22 19:15:35 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   # get 'students/index'
 
   resources :students, except: [:destroy, :index]
+  resources :school_classes, except: [:destroy, :index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
